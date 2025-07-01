920b96a5d570727393fe85501016a961755d8e03
CHAEEUN0 <chaeeun@kyonggi.ac.kr>
2025-07-01 14:56:54 +0900

    Initial commit

---
+++ src/main/java/soulfit/soulfit/SoulfitApplication.java
@@ -0,0 +1,14 @@
+package soulfit.soulfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulfitApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/soulfit/soulfit/SoulfitApplicationTests.java
@@ -0,0 +1,13 @@
+package soulfit.soulfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
